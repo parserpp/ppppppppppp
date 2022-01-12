22570941496c397269047bb58965a78e7fe53bd3
sanbo <sanbo.xyz@gmail.com>
2022-01-12 15:28:22 +0800

    update. at 2022-01-12 15:28:22

--- pkg_list/update_pkg_list.py
+++ pkg_list/update_pkg_list.py
@@ -6,14 +6,15 @@
 获取数据,包名
 """
 import csv
+import sys
+
 import requests
 
 base_wandoujia = "https://www.wandoujia.com/apps/{0}"
 base_yingyongbao = "https://webcdn.m.qq.com/webapp/homepage/index.html#/appDetail?apkName={0}"
 
-
-pkg_file="pkg.csv"
-result_file="pkg_result.csv"
+pkg_file = "pkg.csv"
+result_file = "pkg_result.csv"
 
 pkg_model = {}
 
@@ -32,7 +33,6 @@ class Model(object):
         self.package_name = __package_name
 
 
-
 def readFile():
     with open(pkg_file, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
@@ -42,29 +42,60 @@ def readFile():
             pkg_model[pkg] = m
             # print("add ", pkg, " success. d: ", len(pkg_model))
 
-
 '''
 豌豆荚包含该app,判断后，减少内存的结构。
 '''
-
-
 def requestWandoujia(pkg, url):
-    get
     resp = requests.get(url)
     notFount = resp.text.__contains__("豌豆们没有找到这个页面")
     if notFount:
         pkg_model.pop(pkg)
 
 
-if __name__ == '__main__':
-    getpkgFile()
+
+def work(_token):
     readFile()
     print("内存结构: ", len(pkg_model))
     # 去除没有内存没有值. eg: com.sz.cleanmaster
-    for pkg in pkg_model.keys():
-        requestWandoujia(pkg, base_wandoujia.format(pkg))
+    # for pkg in pkg_model.keys():
+    #     requestWandoujia(pkg, base_wandoujia.format(pkg))
+    ## 其他地方另存结果
+    if len(pkg_model)>0:
+        with open("test.csv", "w") as csvfile:
+            for pkg in pkg_model.keys():
+                model = pkg_model[pkg]
+                appname=model.app_name
+                writer = csv.writer(csvfile)
+                writer.writerow([appname, pkg])
 
 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        work(sys.argv[1])
+    else:
+        print("入参不对,即将停止")
+
+
+#
+#
+# pkg_url = "https://raw.githubusercontent.com/parserpp/data/main/appList.csv?token={0}"
+#
+# """
+# 文件太大无法下载
+# curl -i -H "Authorization: token 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" \
+#     https://api.github.com/repos/parserpp/data/contents/appList.csv
+#
+# Error msg
+# This API returns blobs up to 1 MB in size. The requested blob is too large to fetch via the API,
+#  but you can use the Git Data API to request blobs up to 100 MB in size
+# """
+# def getpkgFile(_token):
+#     resp = requests.get(pkg_url.format(_token))
+#     resp.encoding = 'utf-8'
+#     print(resp.text)
+#     pass
+#
+#
 # def parser_line(line):
 #     # requests.request(url.format())
 #     temp1 = line.strip()
