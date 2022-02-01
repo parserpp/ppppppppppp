4e900faddd5a66a736f2058ec1733686871c5dca
sanbo.xyz <sanbo.xyz@gmail.com>
2022-02-01 18:20:26 +0800

    update wandoujia. wandoujia failed.

--- baseRequest.py
+++ baseRequest.py
@@ -1,7 +1,11 @@
+from datetime import time
+from random import random
+
 import requests
 import urllib3
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 
 urllib3.disable_warnings()
@@ -14,6 +18,16 @@ def request(appname, pkg, url):
     chrome_options.add_argument('--headless')  # 把Chrome浏览器设置为静默模式
     chrome_options.add_argument('--disable-gpu')  # 禁止加载图片
     driver = webdriver.Chrome(options=chrome_options)  # 设置引擎为Chrome，在后台默默运行
+
+    try:
+        action = ActionChains(webdriver)
+        slider = webdriver.find_element_by_xpath("//span[contains(@class, 'nc-lang-cnt')]")
+        action.click_and_hold(slider)  # 按住
+        action.move_by_offset(248, 0)  # 往右偏移248个像素
+        action.release()  # 释放鼠标
+        action.perform()  # 执行
+    except:
+        pass
 
     driver.get(url)
     print("=================webdriver=before[" + appname + "]======================")
@@ -44,38 +58,46 @@ def get(appname, pkg, url):
 
 
 if __name__ == '__main__':
-    print("===============应用已下架，不可分析======================")
-    print("===============应用已下架，不可分析======================")
-    print("===============应用已下架，不可分析======================")
+    # print("===============应用已下架，不可分析======================")
+    # print("===============应用已下架，不可分析======================")
+    # print("===============应用已下架，不可分析======================")
 
-    request("Plugo WiFi-应用已下架，不可分析", "com.way.smsmaster"
-            , "https://www.wandoujia.com/apps/com.way.smsmaster")
-    get("Plugo WiFi-应用已下架，不可分析", "com.way.smsmaster"
-        , "https://www.wandoujia.com/apps/com.way.smsmaster")
+    # request("Plugo WiFi-应用已下架，不可分析", "com.way.smsmaster"
+    #         , "https://www.wandoujia.com/apps/com.way.smsmaster")
+    # get("Plugo WiFi-应用已下架，不可分析", "com.way.smsmaster"
+    #     , "https://www.wandoujia.com/apps/com.way.smsmaster")
 
-    print("===============豌豆们没有找到这个页面(从未有,不可访问)======================")
-    print("===============豌豆们没有找到这个页面(从未有,不可访问)======================")
-    print("===============豌豆们没有找到这个页面(从未有,不可访问)======================")
-    # request("豌豆们没有找到这个页面", "com.nobody.smsmasterxx", "https://www.wandoujia.com/apps/com.nobody.smsmasterxx")
-    # get("豌豆们没有找到这个页面", "com.nobody.smsmasterxx", "https://www.wandoujia.com/apps/com.nobody.smsmasterxx")
+    # print("===============豌豆们没有找到这个页面(从未有,不可访问)======================")
+    # print("===============豌豆们没有找到这个页面(从未有,不可访问)======================")
+    # print("===============豌豆们没有找到这个页面(从未有,不可访问)======================")
+    # # request("豌豆们没有找到这个页面", "com.nobody.smsmasterxx", "https://www.wandoujia.com/apps/com.nobody.smsmasterxx")
+    # # get("豌豆们没有找到这个页面", "com.nobody.smsmasterxx", "https://www.wandoujia.com/apps/com.nobody.smsmasterxx")
 
-    print("===============应用曾上架，现状下架，可继续分析======================")
-    print("===============应用曾上架，现状下架，可继续分析======================")
-    print("===============应用曾上架，现状下架，可继续分析======================")
-    # 异常流量下午分析
-    request(" 语音拨号(应用曾上架，现状下架，可继续分析)", "app.taolessyuyinbohao"
-            , "https://www.wandoujia.com/apps/app.taolessyuyinbohao")
-    get(" 语音拨号(应用曾上架，现状下架，可继续分析)", "app.taolessyuyinbohao"
-        , "https://www.wandoujia.com/apps/app.taolessyuyinbohao")
+    # print("===============应用曾上架，现状下架，可继续分析======================")
+    # print("===============应用曾上架，现状下架，可继续分析======================")
+    # print("===============应用曾上架，现状下架，可继续分析======================")
+    # # 异常流量下午分析
+    # request(" 语音拨号(应用曾上架，现状下架，可继续分析)", "app.taolessyuyinbohao"
+    #         , "https://www.wandoujia.com/apps/app.taolessyuyinbohao")
+    # get(" 语音拨号(应用曾上架，现状下架，可继续分析)", "app.taolessyuyinbohao"
+    #     , "https://www.wandoujia.com/apps/app.taolessyuyinbohao")
 
-    print("===============正常,可分析======================")
-    print("===============正常,可分析=======================")
-    print("===============正常,可分析=======================")
-    request("微信", "com.tencent.mm", "https://www.wandoujia.com/apps/com.tencent.mm")
-    get("微信", "com.tencent.mm", "https://www.wandoujia.com/apps/com.tencent.mm")
+    # print("===============正常,可分析======================")
+    # print("===============正常,可分析=======================")
+    # print("===============正常,可分析=======================")
+    # request("微信", "com.tencent.mm", "https://www.wandoujia.com/apps/com.tencent.mm")
+    # get("微信", "com.tencent.mm", "https://www.wandoujia.com/apps/com.tencent.mm")
 
-    print("===============排行榜======================")
-    print("===============排行榜=======================")
-    print("===============排行榜=======================")
-    request("排行榜", "com.tencent.mm", "https://www.wandoujia.com/top/app")
-    get("排行榜", "com.tencent.mm", "https://www.wandoujia.com/top/app")
+    # print("===============排行榜======================")
+    # print("===============排行榜=======================")
+    # print("===============排行榜=======================")
+    # request("排行榜", "com.tencent.mm", "https://www.wandoujia.com/top/app")
+    # get("排行榜", "com.tencent.mm", "https://www.wandoujia.com/top/app")
+
+
+    print("===============单个分析======================")
+    print("===============单个分析=======================")
+    request(" 超级指南针 电子罗盘器 (应用曾上架，现状下架，可继续分析)", "com.wafcompass"
+            , "https://www.wandoujia.com/apps/com.wafcompass")
+    # get(" 超级指南针 电子罗盘器 (应用曾上架，现状下架，可继续分析)", "com.wafcompass"
+    #     , "https://www.wandoujia.com/apps/com.wafcompass")
--- update_pkg_list_wandoujia.py
+++ update_pkg_list_wandoujia.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-
-
 """
 获取数据,包名
 """
@@ -25,7 +23,8 @@ HEADER = {
     # 'User-Agent': ua.random,
     'Accept': '*/*',
     'Connection': 'keep-alive',
-    'Accept-Language': 'zh-CN,zh;q=0.8'}
+    'Accept-Language': 'zh-CN,zh;q=0.8'
+}
 
 base_wandoujia = "https://www.wandoujia.com/apps/{0}"
 base_yingyongbao = "https://webcdn.m.qq.com/webapp/homepage/index.html#/appDetail?apkName={0}"
@@ -87,11 +86,7 @@ def readFile(_sbegin: int = 0, _steps: int = 0):
 # 豌豆荚包含该app
 def isFountInWandoujia(pkg, url):
     try:
-        resp = requests.get(url
-                            , headers=HEADER
-                            , timeout=3
-                            , verify=False
-                            )
+        resp = requests.get(url, headers=HEADER, timeout=3, verify=False)
 
         if resp.status_code == 200:
             if "该应用已下架" in resp.text:
@@ -101,7 +96,8 @@ def isFountInWandoujia(pkg, url):
                 print("pkg: {0} 相似应用下载  not fount!".format(pkg))
                 return False
             elif "app-offline-down" in resp.text:
-                print("pkg: {0} 图片(安装豌豆荚,下载相似应用)下载链接容器  not fount!".format(pkg))
+                print(
+                    "pkg: {0} 图片(安装豌豆荚,下载相似应用)下载链接容器  not fount!".format(pkg))
                 return False
             elif "image.uc.cn/s/uae/g/0n/wenshan/1.png" in resp.text:
                 print("pkg: {0} 图片链接  not fount!".format(pkg))
@@ -110,7 +106,8 @@ def isFountInWandoujia(pkg, url):
                 print(pkg + "---包含download-wp----200----")
                 return True
             else:
-                print(pkg + "-----200---其他情况-----" + resp.text)
+                # print(pkg + "-----200---其他情况-----" + resp.text)
+                print(pkg + "-----[%s]200---其他情况-----\n %s" % ( pkg, resp.text))
                 return False
         else:
             return False
@@ -120,15 +117,18 @@ def isFountInWandoujia(pkg, url):
     return False
 
 
-def work(_token=os.getenv('GITHUB_TOKEN', ""), _sbegin: int = 0, _step: int = 0):
+def work(
+        _token=os.getenv('GITHUB_TOKEN', ""), _sbegin: int = 0,
+        _step: int = 0):
     readFile(_sbegin, _step)
 
-    print("[%d-%d] 读取文件完毕,内存数量:%d " % (_sbegin, (_sbegin + _step), len(pkg_model)))
+    print("[%d-%d] 读取文件完毕,内存数量:%d " % (_sbegin,
+                                       (_sbegin + _step), len(pkg_model)))
     ## 去除没有内存没有值. eg: com.sz.cleanmaster
     for pkg in pkg_model.keys():
         if isFountInWandoujia(pkg, base_wandoujia.format(pkg)):
             success_pkg_model[pkg] = pkg_model[pkg]
-    print("[%d-%d] 检测完成，成功个数: %d", len(success_pkg_model))
+    print("[%d-%d] 检测完成，成功个数: %d" % (_sbegin, (_sbegin + _step), len(success_pkg_model)))
     if len(success_pkg_model) > 0:
         # 查找关联应用
 
@@ -145,26 +145,20 @@ def work(_token=os.getenv('GITHUB_TOKEN', ""), _sbegin: int = 0, _step: int = 0)
                 except Exception as e:
                     print("[" + pkg + "] happen Exception")
 
-        github_api.create_file(_owner="parserpp"
-                               , _repo="data"
-                               , _path="/pkgs/" + str(_sbegin) + "_" + str(_step) + ".csv"
-                               , _token=_token
-                               , _filename=result_file
-                               , _commit_msg="ppppp"
-                               , _name="who are U"
-                               )
+        # github_api.create_file(_owner="parserpp",
+        #                        _repo="data",
+        #                        _path="/pkgs/" + str(_sbegin) + "_" +
+        #                        str(_step) + ".csv",
+        #                        _token=_token,
+        #                        _filename=result_file,
+        #                        _commit_msg="ppppp",
+        #                        _name="who are U")
     else:
         print("木有成功的啊")
 
 
-"""
-argv：
-   token: github token
-   s1: start_index 开始下标
-   s2: step 步长。持续数量
-"""
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
+def realwork(argv):
+    if len(argv) > 1:
         print("收到多个参数： " + str(sys.argv))
         token = sys.argv[1]
         begin_index = int(sys.argv[2])
@@ -174,7 +168,17 @@ if __name__ == '__main__':
         print("入参不对,即将开启所有的工作模式")
         # work()
 
-    # work("", 0, 10)
+
+"""
+argv：
+   token: github token
+   s1: start_index 开始下标
+   s2: step 步长。持续数量
+"""
+if __name__ == '__main__':
+    # realwork(sys.argv)
+
+    work("", 0, 10)
     # work("", 1000, 1000)
     # work("", 2000, 1000)
     # work("", 3000, 1000)
